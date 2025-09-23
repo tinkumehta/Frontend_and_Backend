f7cd9e169ef667ea15ec92758866ba8368d4bccc
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-09-23 13:26:32 +0530

    Remove unused pages and services from the frontend, including Cart, Home, Login, NotFound, Orders, ProductDetail, Products, Profile, Register, and related API services. This cleanup helps streamline the codebase and improve maintainability.

--- E-commerce/Fronted/src/services/paymentService.js
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export const paymentService = {
-    createOrder : (orderData) => api.post('/paymets/create-order', orderData),
-    verifyPayment: (paymentData) => api.post('/payments/test-payment-verify', paymentData),
-    getUserOrders: () => api.get('/payments/orders'),
-    getOrderById: (id) => api.get(`/payments/orders/{id}`),
-    
-}
