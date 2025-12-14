9792e7f695791e82993e737bbd8e0f80f83e474c
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-12-14 21:57:38 +0530

    feat: initialize backend with package.json and basic structure; add Auth0 for authentication in frontend

--- Hair-queue/forntend/src/App.jsx
+++ Hair-queue/forntend/src/App.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useAuth0 } from '@auth0/auth0-react'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const {loginWithRedirect , user, isAuthenticated, logout} = useAuth0();
 
+  console.log("Current user ", user);
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="app">
+     
+    {isAuthenticated && 
+    <div>
+    <h2>{user.name}</h2>
+    <img src={user.picture} alt="" />
+    </div>
+    }
+      <header className='App-header'>
+        {isAuthenticated ? (
+          <button onClick={(e) => logout()}>Log out</button>
+        ) : (
+          <button onClick={(e) => loginWithRedirect()}>
+          Login With Redireact
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        )}
+        
+      </header>
+    </div>
   )
 }
 
--- Hair-queue/forntend/src/main.jsx
+++ Hair-queue/forntend/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {Auth0Provider} from "@auth0/auth0-react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTHO_DOMAIN}
+      clientId={import.meta.env.VITE_AUTHO_CLIENT_ID}
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
