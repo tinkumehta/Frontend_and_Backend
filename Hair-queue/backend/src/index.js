1974c26bd9b64a9951e76b566997182b14a45fb2
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-12-17 13:55:30 +0530

    feat: add multer for file uploads, implement user routes with JWT verification and middleware integration

---
+++ Hair-queue/backend/src/index.js
@@ -0,0 +1,4 @@
+import dotenv from 'dotevn'
+import mongoose from 'mongoose'
+import { app } from './app'
+
---
+++ Hair-queue/backend/src/routes/user.routes.js
@@ -0,0 +1,29 @@
+import { 
+    changePassword,
+    getUser,
+    loginUser,
+    logoutUser,
+    registerUser
+ } from "../controllers/user.controllers.js"; 
+ import { upload } from "../middlewares/multer.middlewares.js";
+ import { verfiyJWT } from "../middlewares/auth.middlewares.js";
+ import { Router } from "express";
+
+ const router = Router();
+
+ router.route("/register").post(
+    upload.fields([
+        {
+            name : 'avatar',
+            maxCount: 1
+        }
+    ]),
+    registerUser
+ )
+
+ router.route("/login").post(upload.none(), loginUser)
+ router.route("/logout").post(verfiyJWT, logoutUser)
+ router.route("/change-password").post(verfiyJWT, changePassword)
+ router.route("/current-user").get(verfiyJWT, getUser)
+
+ export default router;
