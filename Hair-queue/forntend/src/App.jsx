e408bf7b511dc86299283dd5f502526aa7748369
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2026-01-02 11:26:23 +0530

    feat: integrate Tailwind CSS and refactor authentication component
    - Added Tailwind CSS and related dependencies to package.json.
    - Removed App.css styles and replaced with Tailwind CSS import in index.css.
    - Refactored authentication logic into a new AuthOLogin component.
    - Updated App.jsx to utilize the new AuthOLogin component.
    - Configured Vite to use Tailwind CSS plugin.

--- Hair-queue/forntend/src/App.jsx
+++ Hair-queue/forntend/src/App.jsx
@@ -4,31 +4,13 @@ import { useAuth0 } from '@auth0/auth0-react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const {loginWithRedirect , user, isAuthenticated, logout} = useAuth0();
+ 
 
-  console.log("Current user ", user);
   
   return (
-    <div className="app">
-     
-    {isAuthenticated && 
-    <div>
-    <h2>{user.name}</h2>
-    <img src={user.picture} alt="" />
-    </div>
-    }
-      <header className='App-header'>
-        {isAuthenticated ? (
-          <button onClick={(e) => logout()}>Log out</button>
-        ) : (
-          <button onClick={(e) => loginWithRedirect()}>
-          Login With Redireact
-        </button>
-        )}
-        
-      </header>
-    </div>
+  <>
+  
+  </>
   )
 }
 
